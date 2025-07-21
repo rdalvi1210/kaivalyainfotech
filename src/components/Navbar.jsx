af6f0298b8bc24b58979d10aa032167fa56affbd
rdalvi1210 <rdalvi1210@gmail.com>
2025-07-22 01:45:35 +0530

    Navbar name updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -257,7 +257,9 @@ const Navbar = () => {
               {currentUser && (
                 <div className="flex items-center space-x-2">
                   <User className="w-5 h-5 text-gray-700 dark:text-white" />
-                  <span>{currentUser.name?.split(" ")[0]}</span>
+                  <span className="text-gray-700 dark:text-white">
+                    {currentUser.name?.split(" ")[0]}
+                  </span>
                 </div>
               )}
             </div>
